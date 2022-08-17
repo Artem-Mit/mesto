b5afded48b991713dc5c6f828d1911018fbba3e0
Artem <artem.v.mitin@yandex.ru>
2022-08-17 15:22:58 +0300

    feat: добавить форму импорта новых фотографий

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,49 +1,4 @@
-// Открытие Popup
-let profileEditButton = document.querySelector(`.profile__edit-button`);
-let popup = document.querySelector(`.popup`)
-function popupOpener() {
-  popup.classList.add(`popup_opened`);
-  nameInput.value = nameField.textContent;
-  jobInput.value = jobField.textContent;
-}
-profileEditButton.addEventListener(`click`,popupOpener);
-
-// Закрыть Popup
-let closePopup = document.querySelector(`.popup__close`);
-function popupCloser() {
-  popup.classList.remove(`popup_opened`);
-}
-closePopup.addEventListener(`click`,popupCloser);
-
-
-// Работа формы
-// Находим форму в DOM
-let formElement = document.querySelector(`.popup__container`);
-// Находим поля формы в DOM
-let nameInput = formElement.name;
-let jobInput = formElement.job;
-let nameField = document.querySelector(`.profile__name`);
-let jobField = document.querySelector(`.profile__profession`);
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
-    nameField.textContent = nameInput.value;
-    jobField.textContent = jobInput.value;
-    popupCloser()
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-
+// Добавить дефолтные карточки на страницу
 const initialCards = [
   {
     name: 'Архыз',
@@ -80,5 +35,61 @@ initialCards.forEach(function ({name, link}) {
   photoElement.querySelector(`.element__title`).textContent = name;
   photos.append(photoElement);
 });
+
+
+
+// Открыть Popup
+const editBtn = document.querySelector(`.profile__edit-button`);
+const addBtn = document.querySelector(`.profile__add-button`);
+const profilePopup = document.querySelector(`.profile__popup`);
+const addPopup = document.querySelector(`.add__popup`);
+function opener(popup) {
+  popup.classList.add(`popup_opened`)
+};
+editBtn.addEventListener(`click`, () => opener(profilePopup));
+addBtn.addEventListener(`click`, () => opener(addPopup));
+
+
+// Закрыть Popup
+const profileCloseBtn = document.querySelector(`.profile__close`);
+const addCloseBtn = document.querySelector(`.add__close`);
+function closer(popup) {
+  popup.classList.remove(`popup_opened`)
+};
+profileCloseBtn.addEventListener(`click`, () => closer(profilePopup));
+addCloseBtn.addEventListener(`click`, () => closer(addPopup));
+
+
+
+// Работа формы
+// Находим форму в DOM
+let formElement = document.querySelector(`.popup__container`);
+// Находим поля формы в DOM
+let nameInput = formElement.name;
+let jobInput = formElement.job;
+let nameField = document.querySelector(`.profile__name`);
+let jobField = document.querySelector(`.profile__profession`);
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textContent
+    nameField.textContent = nameInput.value;
+    jobField.textContent = jobInput.value;
+    popupCloser()
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
 
 
