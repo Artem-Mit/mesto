294fa94e20b7a2f906d7df86aad4d2b2ab8348ab
Artem <artem.v.mitin@yandex.ru>
2022-08-17 11:34:50 +0300

    feat: добавить первые фото с помощью JS

--- scripts/popup.js
+++ scripts/popup.js
@@ -44,3 +44,41 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function ({name, link}) {
+  const elementTemplate = document.querySelector(`#element`).content;
+  const photos = document.querySelector(`.photos`);
+  const photoElement = elementTemplate.querySelector(`.element`).cloneNode(true);
+  photoElement.querySelector(`.element__img`).src = link;
+  photoElement.querySelector(`.element__img`).alt = `Фото ${name}`;
+  photoElement.querySelector(`.element__title`).textContent = name;
+  photos.append(photoElement);
+});
+
+
