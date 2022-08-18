1b96ad028eea2e7390dca8a9abfa9b98d2be01a6
Artem <artem.v.mitin@yandex.ru>
2022-08-18 11:40:24 +0300

    feat: добавить импорт новых карточек

--- scripts/popup.js
+++ scripts/popup.js
@@ -26,33 +26,43 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach(function ({name, link}) {
-  const elementTemplate = document.querySelector(`#element`).content;
-  const photos = document.querySelector(`.photos`);
+const addCard = function ({name, link}) {
+  const elementTemplate = document.querySelector(`.element-template`).content;
   const photoElement = elementTemplate.querySelector(`.element`).cloneNode(true);
+  const photos = document.querySelector(`.photos`);
   photoElement.querySelector(`.element__img`).src = link;
   photoElement.querySelector(`.element__img`).alt = `Фото ${name}`;
   photoElement.querySelector(`.element__title`).textContent = name;
   photos.append(photoElement);
-});
+};
+
+initialCards.forEach(addCard);
+
+
 
 
 
 // Открыть Popup
 const editBtn = document.querySelector(`.profile__edit-button`);
 const addBtn = document.querySelector(`.profile__add-button`);
-const profilePopup = document.querySelector(`.profile__popup`);
-const addPopup = document.querySelector(`.add__popup`);
+const profilePopup = document.querySelector(`.profile-popup`);
+const addPopup = document.querySelector(`.add-popup`);
+
 function opener(popup) {
-  popup.classList.add(`popup_opened`)
+  popup.classList.add(`popup_opened`);
+  if (popup == profilePopup) {
+    nameInput.value = nameField.textContent;
+    jobInput.value = jobField.textContent;
+  }
 };
+
 editBtn.addEventListener(`click`, () => opener(profilePopup));
 addBtn.addEventListener(`click`, () => opener(addPopup));
 
 
 // Закрыть Popup
-const profileCloseBtn = document.querySelector(`.profile__close`);
-const addCloseBtn = document.querySelector(`.add__close`);
+const profileCloseBtn = document.querySelector(`.profile-popup__close`);
+const addCloseBtn = document.querySelector(`.add-popup__close`);
 function closer(popup) {
   popup.classList.remove(`popup_opened`)
 };
@@ -63,7 +73,7 @@ addCloseBtn.addEventListener(`click`, () => closer(addPopup));
 
 // Работа формы
 // Находим форму в DOM
-let formElement = document.querySelector(`.popup__container`);
+let formElement = document.querySelector(`.profile-popup__container`);
 // Находим поля формы в DOM
 let nameInput = formElement.name;
 let jobInput = formElement.job;
@@ -82,14 +92,30 @@ function formSubmitHandler (evt) {
     // Вставьте новые значения с помощью textContent
     nameField.textContent = nameInput.value;
     jobField.textContent = jobInput.value;
-    popupCloser()
+    closer(profilePopup)
 }
-
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 
+// Добавление новых карт:
+const addSbmt = document.querySelector(`.add-popup__submit`);
+const addForm = document.querySelector(`.add-popup__container`);
+const imgNameInput = addForm.img;
+const srcInput = addForm.source;
 
+function newCard (evt) {
+  evt.preventDefault();
+  const elementTemplate = document.querySelector(`.element-template`).content;
+  const photoElement = elementTemplate.querySelector(`.element`).cloneNode(true);
+  const photos = document.querySelector(`.photos`);
+  photoElement.querySelector(`.element__img`).src = srcInput.value;
+  photoElement.querySelector(`.element__img`).alt = `Фото ${imgNameInput.value}`;
+  photoElement.querySelector(`.element__title`).textContent = imgNameInput.value;
+  photos.prepend(photoElement);
+  closer(addPopup);
+}
+addForm.addEventListener(`submit`, newCard);
 
